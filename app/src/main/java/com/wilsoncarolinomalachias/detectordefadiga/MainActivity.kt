cbb78ae3526ded1d616bd4ef58fb2796b86fabb6
Bruno Wilson <brunowilson4@gmail.com>
2022-10-19 20:04:12 -0300

    Cria botão

--- app/src/main/java/com/wilsoncarolinomalachias/detectordefadiga/MainActivity.kt
+++ app/src/main/java/com/wilsoncarolinomalachias/detectordefadiga/MainActivity.kt
@@ -3,13 +3,22 @@ package com.wilsoncarolinomalachias.detectordefadiga
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wilsoncarolinomalachias.detectordefadiga.ui.theme.DetectorDeFadigaTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +27,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             DetectorDeFadigaTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier,
+                    color = MaterialTheme.colors.background
+                ) {
+                    TelaInicial()
                 }
             }
         }
@@ -27,14 +39,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TelaInicial() {
+    Box(
+        contentAlignment= Alignment.Center,
+        modifier = Modifier
+            .background(Color.Blue, shape = CircleShape)
+            .size(200.dp)
+    ) {
+        Text(
+            text = "Iniciar corrida",
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            modifier = Modifier.padding(4.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     DetectorDeFadigaTheme {
-        Greeting("Android")
+        TelaInicial()
     }
 }
