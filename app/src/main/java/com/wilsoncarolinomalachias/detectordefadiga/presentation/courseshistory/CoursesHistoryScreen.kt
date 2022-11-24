6dbe86178d2c5c8e8a1ff257700bdad17ca8b98c
c0654092 <c0654092@vale.com>
2022-11-23 23:41:03 -0300

    correções design CourseCard e criação searchBar

--- app/src/main/java/com/wilsoncarolinomalachias/detectordefadiga/presentation/courseshistory/CoursesHistoryScreen.kt
+++ app/src/main/java/com/wilsoncarolinomalachias/detectordefadiga/presentation/courseshistory/CoursesHistoryScreen.kt
@@ -1,8 +1,29 @@
 package com.wilsoncarolinomalachias.detectordefadiga.presentation.courseshistory
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import com.wilsoncarolinomalachias.detectordefadiga.presentation.courseshistory.components.CourseCardGreen
+import com.wilsoncarolinomalachias.detectordefadiga.presentation.courseshistory.components.CourseCardOrange
+import com.wilsoncarolinomalachias.detectordefadiga.presentation.courseshistory.components.SearchAppBar
+import com.wilsoncarolinomalachias.detectordefadiga.presentation.ui.theme.DetectorDeFadigaTheme
 
 @Composable
 fun CoursesHistoryScreen() {
+    Column (verticalArrangement = Arrangement.SpaceBetween, horizontalAlignment = Alignment.CenterHorizontally){
+        SearchAppBar(text = "Busque por corridas feitas", onTextChange = {}, onCloseClicked = {}, onSearchClicked = {})
+        CourseCardGreen()
+        CourseCardOrange()
+        CourseCardOrange()
+    }
+}
 
+@Preview
+@Composable
+fun CourseCardPreview() {
+    DetectorDeFadigaTheme {
+        CoursesHistoryScreen()
+    }
 }
--- app/src/main/java/com/wilsoncarolinomalachias/detectordefadiga/presentation/courseshistory/components/CourseCard.kt
+++ app/src/main/java/com/wilsoncarolinomalachias/detectordefadiga/presentation/courseshistory/components/CourseCard.kt
@@ -16,31 +16,71 @@ import com.wilsoncarolinomalachias.detectordefadiga.presentation.ui.theme.Detect
 
 @Composable
 fun CourseCardGreen() {
+
+    val Verde = Color(0xFF1CC900)
+
     val paddingModifier = Modifier.padding(10.dp)
     val mainButtonColor = ButtonDefaults.buttonColors(
-        backgroundColor = Color.Green,
-
+        backgroundColor = Verde,
     )
-    Card(
-        shape = RoundedCornerShape(20.dp),
-        elevation = 10.dp,
-        border = BorderStroke(3.dp, Color.Green),
-        modifier = paddingModifier
-    ) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Card(
+            shape = RoundedCornerShape(20.dp),
+            elevation = 10.dp,
+            border = BorderStroke(3.dp, Verde),
+            modifier = paddingModifier
+        ) {
 
-        Column(modifier = paddingModifier){
-            Text(text = "Corrida", fontSize = 20.sp)
-            Text(text = "Finalizada em 03/10/2022 às 19:30", fontSize = 14.sp, color = Color.Green)
-            Text(text = "De: Rua João de Paula, Sagrada F. - Belo Horizonte", fontSize = 14.sp)
-            Text(text = "Para: Belo Vale - MG", fontSize = 14.sp)
+            Column(modifier = paddingModifier, verticalArrangement = Arrangement.spacedBy(4.dp)){
+                Text(text = "Corrida", fontSize = 20.sp)
+                Text(text = "Finalizada em 03/10/2022 às 19:30", fontSize = 14.sp, color = Verde)
+                Text(text = "De: Rua João de Paula, Sagrada F. - Belo Horizonte", fontSize = 14.sp)
+                Text(text = "Para: Belo Vale - MG", fontSize = 14.sp)
 
-            Button(colors = mainButtonColor, shape = RoundedCornerShape(50), onClick = { /*TODO*/ }, modifier = Modifier
-                .fillMaxWidth(0.8f)
-                .align(Alignment.CenterHorizontally)
+                Button(colors = mainButtonColor, shape = RoundedCornerShape(50), onClick = { /*TODO*/ }, modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .align(Alignment.CenterHorizontally)
+                )
+                {
+                    Text(text = "GERAR RELATÓRIO", fontSize = 12.sp, color = Color.White)
+                }
+            }
+        }
+    }
 
-            )
-            {
-                Text(text = "GERAR RELATÓRIO", fontSize = 12.sp, color = Color.White)
+}
+
+
+@Composable
+fun CourseCardOrange() {
+    val Laranja = Color(0xFFFF9500)
+
+    val paddingModifier = Modifier.padding(10.dp)
+    val mainButtonColor = ButtonDefaults.buttonColors(
+        backgroundColor = Laranja,
+    )
+    Column() {
+        Card(
+            shape = RoundedCornerShape(20.dp),
+            elevation = 10.dp,
+            border = BorderStroke(3.dp, Laranja),
+            modifier = paddingModifier
+        ) {
+
+            Column(modifier = paddingModifier, verticalArrangement = Arrangement.spacedBy(4.dp)){
+                Text(text = "Corrida", fontSize = 20.sp)
+                Text(text = "Finalizada em 03/10/2022 às 19:30", fontSize = 14.sp, color = Laranja)
+                Text(text = "De: Rua João de Paula, Sagrada F. - Belo Horizonte", fontSize = 14.sp)
+                Text(text = "Para: Belo Vale - MG", fontSize = 14.sp)
+
+                Button(colors = mainButtonColor, shape = RoundedCornerShape(50), onClick = { /*TODO*/ }, modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .align(Alignment.CenterHorizontally)
+
+                )
+                {
+                    Text(text = "GERAR RELATÓRIO", fontSize = 12.sp, color = Color.White)
+                }
             }
         }
     }
@@ -50,6 +90,6 @@ fun CourseCardGreen() {
 @Composable
 fun CourseCardPreview() {
     DetectorDeFadigaTheme {
-        CourseCardGreen()
+        CourseCardOrange()
     }
 }
