d4e136d34ffc6a384ec7b8c677a09f89a212929e
c0654092 <c0654092@vale.com>
2022-12-01 20:49:04 -0300

    correção top app bar

--- app/src/main/java/com/wilsoncarolinomalachias/detectordefadiga/presentation/courseshistory/CoursesHistoryScreen.kt
+++ app/src/main/java/com/wilsoncarolinomalachias/detectordefadiga/presentation/courseshistory/CoursesHistoryScreen.kt
@@ -1,30 +1,34 @@
-package com.wilsoncarolinomalachias.detectordefadiga.presentation.courseshistory
+package com.wilsoncarolinomalachias.detectordefadiga.presentation.courseshistory.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
-import com.wilsoncarolinomalachias.detectordefadiga.presentation.courseshistory.components.CourseCardGreen
-import com.wilsoncarolinomalachias.detectordefadiga.presentation.courseshistory.components.CourseCardOrange
-import com.wilsoncarolinomalachias.detectordefadiga.presentation.courseshistory.components.ScaffoldSample
-import com.wilsoncarolinomalachias.detectordefadiga.presentation.courseshistory.components.SearchAppBar
 import com.wilsoncarolinomalachias.detectordefadiga.presentation.ui.theme.DetectorDeFadigaTheme
+
 
 @Composable
 fun CoursesHistoryScreen() {
-    Column (verticalArrangement = Arrangement.SpaceBetween, horizontalAlignment = Alignment.CenterHorizontally){
-//        ScaffoldSample()
-        SearchAppBar(text = "Busque por corridas feitas", onTextChange = {}, onCloseClicked = {}, onSearchClicked = {})
-        CourseCardGreen()
-        CourseCardOrange()
-        CourseCardOrange()
-    }
+    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = { TopAppBar(title = {Text("Top App Bar")},backgroundColor = MaterialTheme.colors.primary)  },
+        floatingActionButtonPosition = FabPosition.End,
+        content = {
+            Column (verticalArrangement = Arrangement.SpaceBetween, horizontalAlignment = Alignment.CenterHorizontally){
+                SearchAppBar(text = "Busque por corridas feitas", onTextChange = {}, onCloseClicked = {}, onSearchClicked = {})
+                CourseCardGreen()
+                CourseCardOrange()
+                CourseCardOrange()
+            }
+        },
+    )
 }
-
 @Preview
 @Composable
-fun CourseCardPreview() {
+fun CoursesHistoryPreview() {
     DetectorDeFadigaTheme {
         CoursesHistoryScreen()
     }
