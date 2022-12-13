f0ce1e3f737c37d6a96a0dc265c40749335aa1e6
Bruno Wilson <brunowilson4@gmail.com>
2022-12-13 19:36:55 -0300

    Adiciona ConstraintLayout e solução da internet pra linha do tempo

--- app/src/main/java/com/wilsoncarolinomalachias/detectordefadiga/presentation/coursereport/CourseReportScreen.kt
+++ app/src/main/java/com/wilsoncarolinomalachias/detectordefadiga/presentation/coursereport/CourseReportScreen.kt
@@ -1,9 +1,55 @@
 package com.wilsoncarolinomalachias.detectordefadiga.presentation.coursereport
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.wilsoncarolinomalachias.detectordefadiga.presentation.Screen
+import com.wilsoncarolinomalachias.detectordefadiga.presentation.courseshistory.components.CourseCardGreen
+import com.wilsoncarolinomalachias.detectordefadiga.presentation.courseshistory.components.CourseCardOrange
+import com.wilsoncarolinomalachias.detectordefadiga.presentation.courseshistory.components.SearchAppBar
 
 @Composable
-fun CourseReportScreen() {
+fun CourseReportScreen(
+    navController: NavHostController
+) {
+    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
 
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Relatórios")
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            navController.navigateUp()
+                        }
+                    ) {
+                        Icon(Icons.Filled.ArrowBack, "backIcon")
+                    }
+                },
+                backgroundColor = Color(0xFF001CBB),
+                contentColor = Color.White
+            )
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        content = {
+            ReportCard(
+                "Rua João de Paula, Sagrada F. - Belo Horizonte",
+                "Belo Vale = MG"
+            )
+        }
+    )
 }
--- app/src/main/java/com/wilsoncarolinomalachias/detectordefadiga/presentation/coursereport/ReportCard.kt
+++ app/src/main/java/com/wilsoncarolinomalachias/detectordefadiga/presentation/coursereport/ReportCard.kt
@@ -1,6 +1,9 @@
 package com.wilsoncarolinomalachias.detectordefadiga.presentation.coursereport
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -12,6 +15,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.wilsoncarolinomalachias.detectordefadiga.presentation.ui.theme.DetectorDeFadigaTheme
 
 @Composable
@@ -21,32 +26,59 @@ fun ReportCard(
 ) {
 
     val brownColor = Color(0xFF734D15)
-
-    val mainButtonColor = ButtonDefaults.buttonColors(
-        backgroundColor = brownColor,
-    )
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+    val visitedAddresses = listOf<String>()
+    
+    Card(
+        shape = RoundedCornerShape(0.dp),
+        elevation = 2.dp,
     ) {
-        Card(
-            shape = RoundedCornerShape(20.dp),
-            elevation = 10.dp,
+        Column {
+            Text(
+                text = "Corrida (Fadiga detectada)",
+                fontSize = 20.sp,
+                color = brownColor
+            )
+
+            Text(
+                text = "De: $courseStartAddress",
+                fontSize = 14.sp
+            )
+
+            Text(
+                text = "Para: $courseDestinationAddress",
+                fontSize = 14.sp
+            )
+        }
+
+        ConstraintLayout(
             modifier = Modifier.padding(10.dp)
         ) {
-
-            Column(modifier = Modifier.padding(10.dp), verticalArrangement = Arrangement.spacedBy(4.dp)){
-                Text(text = "Corrida (Fadiga detectada)", fontSize = 20.sp, color = brownColor)
-                Text(text = "De: $courseStartAddress", fontSize = 14.sp)
-                Text(text = "Para: $courseDestinationAddress", fontSize = 14.sp)
-
-                Row() {
-                    Icon(Icons.Rounded.LocationOn, contentDescription = "Localized description", tint = Color.Blue)
-                    Text(text = "De: $courseStartAddress", fontSize = 14.sp)
+            val (timelineRef,textRef) = createRefs()
+            Spacer(
+                modifier = Modifier
+                    .constrainAs(timelineRef) {
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start, 10.dp)
+                        height = Dimension.fillToConstraints
+                    }
+                    .background(color = Color.Blue)
+                    .width(2.dp)
+            )
+            LazyColumn(
+                modifier = Modifier.constrainAs(textRef) {
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                start.linkTo(timelineRef.start,10.dp)
+                end.linkTo(parent.end)
+            }) {
+                items(visitedAddresses) { address ->
+                    Text(address, fontSize = 14.sp)
                 }
             }
         }
-    }
 
+    }
 }
 
 @Preview
